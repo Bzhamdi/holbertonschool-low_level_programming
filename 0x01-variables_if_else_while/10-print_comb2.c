ca860e0fb0ca0fc602ecffbcec94a8a5b0a2a950
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-02-06 16:15:18 +0000

    hamdi

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c;
+for (c = 0; c <= 99; ++c)
+{
+putchar (c / 10 + '0');
+putchar(c % 10 + '0');
+if (c != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
