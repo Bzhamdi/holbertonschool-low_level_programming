608be4ff1323f5fe036d8416ed1f6afd596b91dd
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-02-06 18:36:01 +0000

    comb_4

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, a, b, c, d;
+for (i = 1; i <= 9899; ++i)
+{
+d = i % 10;
+c = i % 100 / 10;
+b = i % 1000 / 100;
+a = i / 1000;
+putchar(d + '0');
+putchar(c + '0');
+putchar(' ');
+putchar(b + '0');
+putchar(a + '0');
+if (c != 9 || a != 9 || b != 8 || d != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
