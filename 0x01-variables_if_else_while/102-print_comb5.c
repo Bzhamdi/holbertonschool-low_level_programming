1bd9231b23ef700994e8d861ade894ae9e18f536
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-02-06 18:50:29 +0000

    hamdi

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,11 +13,11 @@ d = i % 10;
 c = i % 100 / 10;
 b = i % 1000 / 100;
 a = i / 1000;
-putchar(d + '0');
-putchar(c + '0');
-putchar(' ');
-putchar(b + '0');
 putchar(a + '0');
+putchar(b + '0');
+putchar(' ');
+putchar(c + '0');
+putchar(d + '0');
 if (c != 9 || a != 9 || b != 8 || d != 9)
 {
 putchar(',');
