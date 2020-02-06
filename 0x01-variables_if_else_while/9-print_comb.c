650facdcdd014f2811592bf255ac86fd95ff621c
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-02-06 15:57:56 +0000

    hamdi

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,12 +7,15 @@
 int main(void)
 {
 int c;
-for (c = 0; c <= 8; ++c)
+for (c = 0; c <= 9; ++c)
 {
 putchar(c % 10 + '0');
+if (c != 9)
+{
 putchar(',');
+putchar(' ');
 }
-putchar('9');
+}
 putchar('\n');
 return (0);
 }
