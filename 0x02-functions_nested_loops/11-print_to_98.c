dce6cde3805f6b4ddcfb93d40cc99eb6e0fc9ccf
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-02-11 11:56:49 +0000

    hamdi

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_to_98 - print number to 98
+ * @n: number recieved (param)
+ *
+ *
+ */
+void print_to_98(int n)
+{
+int i;
+if (n < 98)
+{
+for (i = n; i <= 98; ++i)
+{
+printf("%d", i);
+if (i != 98)
+printf(", ");
+}
+printf("\n");
+}
+if (n == 0)
+{
+printf("%d\n", n);
+}
+else
+{
+for (i = n; i >= 98; --i)
+{
+printf("%d", i);
+if (i != 98)
+printf(", ");
+}
+printf("\n");
+}
+}
