c7fb3b62fc6329b41158c8e60e2b2644a797fd5a
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-02-11 10:58:09 +0000

    hamdi

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *add - add_2_int
+ *
+ *
+ *@a: number recieved (param)
+ *@b: number recieved (param)
+ *Return: add
+ */
+int add(int a, int b)
+{
+int z;
+z = a + b;
+return (z);
+}
