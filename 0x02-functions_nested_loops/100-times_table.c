b88c2b99f2572306a8d2d713d288f3b37848560c
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-02-12 11:04:54 +0000

    hamdi

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,10 +21,12 @@ c = j * i;
 if (c >= 10 && c < 100)
 {
 _putchar(' ');
+_putchar(' ');
 _putchar(c / 10 + '0');
 _putchar(c % 10 + '0');
 }
 else if (c >= 100)
+
 {
 _putchar(' ');
 _putchar(c / 100 + '0');
@@ -35,6 +37,7 @@ else
 {
 if (j != 0)
 {
+_putchar(' ');
 _putchar(' ');
 _putchar(' ');
 }
