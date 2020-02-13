c86f1f36d3ef238fe3dd67d77f9273509a0784bc
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-02-13 16:58:55 +0000

    hamdi

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ *print_line - prints
+ *@size: param
+ *
+ *
+ */
+void print_square(int size)
+{
+int j, i;
+if (size > 0)
+{
+for (i = 0; i < size; ++i)
+{
+for (j = 0; j < size; ++j)
+_putchar ('#');
+_putchar ('\n');
+}
+}
+else
+_putchar ('\n');
+}
