cd275c3bae9d09679d5b079f69a059150d5ae799
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-02-13 22:03:15 +0000

    hamdi

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,21 +12,9 @@ if (n < 0)
 _putchar('-');
 n = -n;
 }
-
-if (n < 100 && n > 9)
+if (n / 10 > 0)
 {
-_putchar(n / 10 + '0');
-}
-if (n > 100 && n < 1000)
-{
-_putchar(n / 100 + '0');
-_putchar(n % 100 / 10 + '0');
-}
-if (n >= 1000)
-{
-_putchar(n / 1000 + '0');
-_putchar(n % 1000 / 100 + '0');
-_putchar(n % 100 / 10 + '0');
+print_number(n / 10);
 }
 _putchar(n % 10 + '0');
 }
