90d00868ec02ca2f0c964b81ededa64977862a07
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-02-13 18:44:59 +0000

    triangle left

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ *print_triangle - prints triangel
+ *@size: param
+ *
+ *
+ */
+void print_triangle(int size)
+{
+int j, i;
+if (size > 0)
+{
+for (i = 0; i <= size; i++)
+{
+for (j = 0; j <= size; j++)
+{
+if (j <= size - i)
+_putchar(' ');
+else
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+_putchar('\n');
+}
