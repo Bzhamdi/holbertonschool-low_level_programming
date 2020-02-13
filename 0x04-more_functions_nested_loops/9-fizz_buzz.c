720a282ddc017d871a46288503ca1f7b337b3444
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-02-13 17:38:47 +0000

    hamdi

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include<stdio.h>
+/**
+ * main - Fizz_Buzz
+ *
+ *
+ */
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+if (i % 3 != 0 && i % 5 != 0)
+{
+printf("%d ", i);
+}
+}
+putchar('\n');
+return (0);
+}
