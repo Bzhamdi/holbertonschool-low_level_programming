02053f7e05ddf4f4d0ebff138612c25e8d76ded9
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-02-18 13:12:22 +0000

    hamdi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ *_atoi - covert char to int
+ *@s: param
+ *
+ *Return: s
+ */
+int _atoi(char *s)
+{
+int i, k, a;
+i=0;
+a = 1;
+k = 0;
+for ( i = 0 ; s[i] != '\0'; ++i)
+{
+if (s[i] == '-')
+{
+a = -a;
+}
+}
+for (i = 0 ; s[i] != '\0'; ++i)
+{
+
+if (s[i] >= '0' && s[i] <= '9')
+{
+k = k * 10;
+k = k + s[i] - '0';
+if (s[i+1] >= '0' && s[i] <= '9')
+continue;
+else
+break;
+}
+}
+return (k * a);
+}
