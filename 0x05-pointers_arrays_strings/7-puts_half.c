6e0f5884ac7ce73e62e899442e99928480a3ae55
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-02-18 09:07:57 +0000

    hamdi

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,10 +9,13 @@ int i, j, a, b;
 a = 0;
 for (i = 0; str[i] != '\0'; i++)
 a = a + 1;
-if (a % 2 != 0)
-a--;
 b = a;
-a = a / 2;
+if (a % 2 != 0)
+{
+a = (a - 1) / 2;
+}
+else
+a = a / 2; 
 for (j = a; j <= b ; j++)
 _putchar (*(str + j));
 _putchar ('\n');
