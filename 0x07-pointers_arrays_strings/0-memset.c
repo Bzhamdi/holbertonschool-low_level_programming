e8102e7855b6f36714aab6c127f2fb09041dc696
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-02-24 11:48:37 +0000

    Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *_memset - change the adr
+ *@s: param
+ *@b: param
+ *@n:param
+ *
+ *Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+unsigned int a;
+a = n;
+i = 0;
+for (i = 0; i <= a; i++)
+s[i] = b;
+return (s);
+}
