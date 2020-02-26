5ffda7970b62be98d61b467dabf96754bd0bbe58
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-02-26 21:04:59 +0000

    sqrt rec

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
 #include "holberton.h"
 /**
- * sqrt - Tests a
- * @a: The number to compare with b
- * @b: The squared number to find the square root of
+ * _sqrt - Tests a
+ * @a: The number to test with b
+ * @b: The squared number to find the square
  *
- * Return: -1 and a otherwise
+ * Return: -1 else a
  */
-int sqrt(int a, int b)
+
+int _sqrt(int a, int b)
 {
 if (a > b)
 return (-1);
 if ((a * a) == b)
 return (a);
 else
-return (sqrt(++a, b));
+return (_sqrt(++a, b));
 }
+
 /**
- * _sqrt_recursion - natural square
- * @n: number
+ * _sqrt_recursion - natural square root
+ * @n: The number
  *
- * Return: -1 else the square root of n
+ * Return: -1 and else the square root of n
  */
+
 int _sqrt_recursion(int n)
 {
 if (n == 0)
 return (-1);
-return (sqrt(1, n));
+return (_sqrt(1, n));
 }
