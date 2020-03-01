ac89e3e1bb0195bee1a9325945117661d5820bb2
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-03-01 22:12:50 +0000

    prints the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main -  prints the number of arguments passed into it
+ *@argc: param int lenth
+ *@argv:param
+ *
+ *Return: int
+ */
+int main(int argc, char **argv)
+{
+if (argv != '\0')
+{
+printf("%d\n", argc - 1);
+}
+return (0);
+}
