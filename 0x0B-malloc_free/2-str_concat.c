7bfa1e7b04cb1eef8d45d05a81963c545f9176c0
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-03-03 21:40:53 +0000

    concatenates two strings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,7 +20,10 @@ while (s1[i])
 i++;
 while (s2[j])
 j++;
+if (i == 0 && j == 0)
+return (NULL);
 ar = malloc((i + j + 1) * sizeof(char));
+return (NULL);
 if (ar == NULL)
 return (NULL);
 for (a = 0; a < i; a++)
@@ -31,6 +34,5 @@ for (; a < i + j; a++)
 ar[a] = s2[b];
 b++;
 }
-ar[i + j] = '\0';
 return (ar);
 }
