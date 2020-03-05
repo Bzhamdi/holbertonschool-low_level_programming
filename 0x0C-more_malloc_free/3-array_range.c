828a118414586883d46782921b75d118f29c15e6
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-03-05 21:41:08 +0000

    creates an array of integers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,13 +10,17 @@
 int *array_range(int min, int max)
 {
 int *ar;
-int i;
+int i, a;
 if (min > max)
 return (NULL);
 ar = malloc((max - min + 1) * sizeof(int));
 if (ar == NULL)
 return (NULL);
-for (i = min; i <= max; i++)
-ar[i] = i;
+a = min;
+for (i = 0; i <= max - min; i++)
+{
+ar[i] = a;
+a++;
+}
 return (ar);
 }
