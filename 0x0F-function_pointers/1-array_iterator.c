24fe0b4db77f7030989bfe76443574b5ec915e59
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-03-11 16:35:22 +0000

    executes a function given

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * array_iterator - executes a function size times.
+ * @array: array
+ * @size: size of array.
+ * @action:pointer of function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+unsigned int i = 0;
+if (array != NULL || action != NULL)
+{
+for (; i < size; i++)
+action(array[i]);
+}
+}
