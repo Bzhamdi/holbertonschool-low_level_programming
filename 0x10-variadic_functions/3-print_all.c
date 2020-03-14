7ceacfce2eb42210e8b79a981c4fa4ac04d61497
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-03-14 22:09:48 +0000

    prints anything.

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ *print_all -  prints all.
+ *@format: types
+ *
+ *
+ */
+void print_all(const char * const format, ...)
+{
+va_list l_args;
+unsigned  int i = 0;
+char *str;
+va_start(l_args, format);
+if (format == NULL)
+return;
+while (format[i] != '\0')
+{
+switch (format[i])
+{
+case 'c':
+printf("%c", (char) va_arg(l_args, int));
+break;
+case 'i':
+printf("%d", va_arg(l_args, int));
+break;
+case 'f':
+printf("%f", (float) va_arg(l_args, double));
+break;
+case 's':
+str = va_arg(l_args, char *);
+if (str == NULL)
+{
+printf("(nil)");
+break;
+}
+printf("%s", str);
+break;
+}
+if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f'
+|| format[i] == 's') && (format[i + 1] != '\0'))
+printf(", ");
+i++;
+}
+va_end(l_args);
+printf("\n");
+}
