b869289d6170704040eac54c38cfe775aedcf920
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-03-24 17:28:02 +0000

    get_node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -6,19 +6,15 @@
  * Return: nth node
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
-
 {
-	unsigned int i;
+	unsigned int i = 0;
 
 	if (head == NULL)
 		return (NULL);
 	for (i = 0; i < index; i++)
-	{
 		head = head->next;
 		if (head)
-
 			return (head);
-	}
-	return (NULL);
+		return (NULL);
 }
 
