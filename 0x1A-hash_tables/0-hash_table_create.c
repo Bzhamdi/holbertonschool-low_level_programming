74f6d257481546bc813484700f8161fb1202b7e1
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-05-29 15:03:58 +0000

    free prob

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash->array = malloc(size * sizeof(hash_node_t *));
 	if (!hash->array)
 	{
-		free(hash);
+
 		return (NULL);
 	}
 	hash->size = size;
