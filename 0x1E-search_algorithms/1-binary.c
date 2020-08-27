df9787631f12f038c9c24cd34c16fb7eb3053a40
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-08-27 05:00:06 -0400

    search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -14,9 +14,10 @@ int binarySearch(int arr[], size_t l, size_t r, int x)
 {
 	size_t i, mid;
 
+	printf("Searching in array: ");
 	for (i = l; i <= r; i++)
 	{
-		printf("Searching in array: ");
+
 		printf("%d", arr[i]);
 		if (i < r)
 			printf(", ");
@@ -24,14 +25,15 @@ int binarySearch(int arr[], size_t l, size_t r, int x)
 			printf("\n");
 	}
 
-	if (r >= l)
+	if (r > l)
 	{
 		mid = l + (r - l) / 2;
 		if (arr[mid] == x)
 			return (mid);
 		if (arr[mid] > x)
 			return (binarySearch(arr, l, mid - 1, x));
-		return (binarySearch(arr, mid + 1, r, x));
+		if (arr[mid] < x)
+			return (binarySearch(arr, mid + 1, r, x));
 	}
 
 	return (-1);
